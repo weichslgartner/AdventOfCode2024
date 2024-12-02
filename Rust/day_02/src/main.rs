8b48c9f9d062f889883ecb79848992eb6b66791e
weichslgartner <weichslgartner@gmail.com>
2024-12-02 20:55:06 +0100

    day 2 rust refactor

--- Rust/day_02/src/main.rs
+++ Rust/day_02/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 
 fn safe(diffs: &[i32]) -> bool {
     let all_positive = diffs.iter().all(|&x| x > 0);
@@ -6,29 +7,43 @@ fn safe(diffs: &[i32]) -> bool {
     in_range && (all_positive || all_negative)
 }
 
-fn adjacent_difference(level: &[i32]) -> Vec<i32> {
-    level.windows(2).map(|w| w[0] - w[1]).collect()
+pub fn skip_nth<'a, I>(iter: I, n: usize) -> impl Iterator<Item = &'a i32>
+where
+    I: Iterator<Item = &'a i32>,
+{
+    iter.into_iter()
+        .enumerate()
+        .filter_map(move |(index, item)| if index == n { None } else { Some(item) })
+}
+
+fn adjacent_difference<'a, I>(level: I) -> Vec<i32>
+where
+    I: Iterator<Item = &'a i32>,
+{
+    level.tuple_windows().map(|(a, b)| b - a).collect()
 }
 
 fn damp(level: &[i32]) -> bool {
-    (0..level.len()).any(|i| {
-        let mut reduced = level.to_vec();
-        reduced.remove(i);
-        safe(&adjacent_difference(&reduced))
-    })
+    (0..level.len()).any(|i| safe(&adjacent_difference(skip_nth(level.iter(), i))))
 }
 
 fn part_1(levels: &[Vec<i32>]) -> usize {
-    levels.iter().filter(|&level| safe(&adjacent_difference(level))).count()
+    levels
+        .iter()
+        .filter(|&level| safe(&adjacent_difference(level.iter())))
+        .count()
 }
 
 fn part_2(levels: &[Vec<i32>]) -> usize {
-    levels.iter().filter(|&level| safe(&adjacent_difference(level)) || damp(level)).count()
+    levels
+        .iter()
+        .filter(|&level| safe(&adjacent_difference(level.iter())) || damp(level))
+        .count()
 }
 
 fn main() {
     let input = include_str!("../../../inputs/input_02.txt");
-    let levels = aoc::parse_ints::<i32,' '>(input).expect("Failed to parse input");
+    let levels = aoc::parse_ints::<i32, ' '>(input).expect("Failed to parse input");
 
     println!("Part 1: {}", part_1(&levels));
     println!("Part 2: {}", part_2(&levels));
