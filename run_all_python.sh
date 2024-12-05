e869e8b09b921dfe83c3a82e82cad0d6136ff018
weichslgartner <weichslgartner@gmail.com>
2024-12-05 17:12:53 +0100

    exec times
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- run_all_python.sh
+++ run_all_python.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# activate correct conda environment
 for file in Python/day*.py
 do
     echo ${file}
-    time python3 ./${file}
+    time /home/andreas/miniforge3/envs/py312/bin/python ./${file}
     printf "\n"
 done
