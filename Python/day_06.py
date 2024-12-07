ca8fa4e9c0f195e0cd11fa97bcb9f94992940546
weichslgartner <weichslgartner@gmail.com>
2024-12-07 15:56:19 +0100

    day 06 part2 refactor
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_06.py
+++ Python/day_06.py
@@ -1,6 +1,6 @@
-from typing import Set
+from typing import Set, List
 
-from aoc import *
+from aoc import Point, Direction, dir_to_point, get_lines
 
 
 def parse_input(lines: List[str]) -> (Set[Point], Point, Point):
@@ -16,20 +16,11 @@ def parse_input(lines: List[str]) -> (Set[Point], Point, Point):
     return obstacles, start, Point(x=len(lines[0]), y=len(lines))
 
 
-def part_1(obstacles: Set[Point], start: Point, max_p: Point) -> int:
-    cur = start
-    visited = set()
-    direction = Direction.NORTH
-    while 0 <= cur.x < max_p.x and 0 <= cur.y < max_p.y:
-        visited.add(cur)
-        direction, cur = get_next_point(cur, direction, obstacles)
-    return len(visited)
-
-
-def part_2(obstacles: Set[Point], start: Point, max_p: Point) -> int:
+def solve(obstacles: Set[Point], start: Point, max_p: Point) -> (int, int):
     cur = start
     new_obstacles = set()
     direction = Direction.NORTH
+    visited = set()
     while 0 <= cur.x < max_p.x and 0 <= cur.y < max_p.y:
         dir_, next_p = get_next_point(cur, direction, obstacles)
         if next_p not in obstacles and next_p not in new_obstacles and next_p != start:
@@ -39,7 +30,7 @@ def part_2(obstacles: Set[Point], start: Point, max_p: Point) -> int:
             obstacles.remove(next_p)
         cur = next_p
         direction = dir_
-    return len(new_obstacles)
+    return len(visited),len(new_obstacles)
 
 
 def has_loops(cur: Point, direction: Direction, max_p: Point, obstacles: Set[Point]) -> bool:
@@ -65,8 +56,9 @@ def get_next_point(cur: Point, direction: Direction, obstacles: Set[Point]) -> (
 def main():
     lines = get_lines("input_06.txt")  # too low 1696 #too high 1922
     obstacles, start, max_p = parse_input(lines)
-    print("Part 1:", part_1(obstacles, start, max_p))
-    print("Part 2:", part_2(obstacles, start, max_p))
+    part_1, part_2 = solve(obstacles, start, max_p)
+    print("Part 1:", part_1)
+    print("Part 2:", part_2)
 
 
 if __name__ == '__main__':
--- Rust/day_06/src/main.rs
+++ Rust/day_06/src/main.rs
@@ -67,62 +67,34 @@ fn parse_input(lines: Vec<&str>) -> (HashSet<Point>, Point, Point) {
     )
 }
 
-fn part_1(obstacles: &HashSet<Point>, start: Point, max_p: Point) -> usize {
-    let mut cur = start;
-    let mut visited = HashSet::new();
-    let mut dir = Direction::North;
 
-    while cur.x >= 0 && cur.x < max_p.x && cur.y >= 0 && cur.y < max_p.y {
-        let mut next = dir.to_point();
-        let mut next_p = Point {
-            x: cur.x + next.x,
-            y: cur.y + next.y,
-        };
-
-        while obstacles.contains(&next_p) {
-            dir = dir.next();
-            next = dir.to_point();
-            next_p = Point {
-                x: cur.x + next.x,
-                y: cur.y + next.y,
-            };
-        }
-        visited.insert(cur);
-        cur = next_p;
-    }
-    visited.len()
-}
-
-fn part_2(obstacles: &mut HashSet<Point>, start: Point, max_p: Point) -> usize {
+fn solve(obstacles: &mut HashSet<Point>, start: Point, max_p: Point) -> (usize,usize) {
     let mut cur = start;
     let mut new_obstacles = HashSet::new();
     let mut dir = Direction::North;
+    let mut visited = HashSet::new();
     while cur.x >= 0 && cur.x < max_p.x && cur.y >= 0 && cur.y < max_p.y {
         let (new_dir, next_p) = get_next_point(cur, dir, obstacles);
         if !obstacles.contains(&next_p) && !new_obstacles.contains(&next_p) && next_p != start {
             obstacles.insert(next_p);
-
             if has_loops(start, Direction::North, max_p, obstacles) {
                 new_obstacles.insert(next_p);
             }
-
             obstacles.remove(&next_p);
         }
+        visited.insert(cur);
         cur = next_p;
         dir = new_dir;
     }
-    new_obstacles.len()
+    (visited.len(),new_obstacles.len())
 }
 
 fn has_loops(mut cur: Point, mut dir: Direction, max_p: Point, obstacles: &HashSet<Point>) -> bool {
     let mut visited_dir = HashSet::new();
     while cur.x >= 0 && cur.x < max_p.x && cur.y >= 0 && cur.y < max_p.y {
         visited_dir.insert((cur, dir));
-
         let (new_dir, next_p) = get_next_point(cur, dir, obstacles);
         if visited_dir.contains(&(next_p, new_dir)) {
-            let next_next = get_next_point(next_p, new_dir, obstacles);
-            assert!(visited_dir.contains(&(next_next.1, next_next.0)));
             return true;
         }
         cur = next_p;
@@ -151,16 +123,14 @@ fn get_next_point(
             y: cur.y + next.y,
         };
     }
-
     (dir, next_p)
 }
 
 fn main() {
     let input = include_str!("../../../inputs/input_06.txt");
     let lines: Vec<&str> = input.lines().collect();
-
     let (mut obstacles, start, max_p) = parse_input(lines);
-
-    println!("Part 1: {}", part_1(&obstacles, start, max_p));
-    println!("Part 2: {}", part_2(&mut obstacles, start, max_p));
+    let (part_1,part_2) = solve(&mut obstacles, start, max_p);
+    println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_2);
 }
