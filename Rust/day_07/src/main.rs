b4ec2d38a81e2547abf5cbaadd5870dd32d15360
weichslgartner <weichslgartner@gmail.com>
2024-12-07 15:57:05 +0100

    day 07 rust
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

---
+++ Rust/day_07/src/main.rs
@@ -0,0 +1,14 @@
+fn part_1(input: &str) -> &str {
+    input
+}
+
+fn part_2(input: &str) -> &str {
+    input
+}
+
+fn main() {
+    let input = include_str!("../../../inputs/input_07.txt");
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
