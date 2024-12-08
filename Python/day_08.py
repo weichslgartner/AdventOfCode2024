ad674fb07b29a849a95154f3302a6698028da0f7
weichslgartner <weichslgartner@gmail.com>
2024-12-08 11:28:56 +0100

    day 08 python
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_08.py
+++ Python/day_08.py
@@ -1,38 +1,51 @@
 from collections import defaultdict
 from itertools import combinations
 
-from aoc import  *
+from aoc import *
 
 
-def parse_input(lines ):
+def parse_input(lines):
     antennas = defaultdict(list)
     points = {}
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
-            if c=="#":
+            if c == "#":
                 continue
             if c != ".":
-                antennas[c].append(Point(x=x,y=y))
-                points[Point(x=x,y=y)] = c
-    return antennas, Point(x=len(lines[0]), y=len(lines)),points
+                antennas[c].append(Point(x=x, y=y))
+                points[Point(x=x, y=y)] = c
+    return antennas, Point(x=len(lines[0]), y=len(lines)), points
 
-def find_antitodes(p1:Point,p2:Point):
-    r1 = Point(x=p1.x+(p1.x-p2.x),y=p1.y+(p1.y-p2.y))
-    r2 = Point(x=p2.x+(p2.x-p1.x),y=p2.y+(p2.y-p1.y))
-    return r1,r2
 
-def part_1(antennas, max_p,points):
+def find_antitodes(p1: Point, p2: Point):
+    r1 = Point(x=p1.x + (p1.x - p2.x), y=p1.y + (p1.y - p2.y))
+    r2 = Point(x=p2.x + (p2.x - p1.x), y=p2.y + (p2.y - p1.y))
+    return r1, r2
+
+
+def find_antitodes2(p1: Point, p2: Point, max_p):
+    antitodes = {p1, p2}
+    r1 = Point(x=p1.x + (p1.x - p2.x), y=p1.y + (p1.y - p2.y))
+    while is_in_grid(r1, max_p):
+        antitodes.add(r1)
+        r1 = Point(x=r1.x + (p1.x - p2.x), y=r1.y + (p1.y - p2.y))
+    r2 = Point(x=p2.x + (p2.x - p1.x), y=p2.y + (p2.y - p1.y))
+    while is_in_grid(r2, max_p):
+        antitodes.add(r2)
+        r2 = Point(x=r2.x + (p2.x - p1.x), y=r2.y + (p2.y - p1.y))
+    return antitodes
+
+
+def part_1(antennas, max_p, points):
     antitodes = set()
-    print(antennas)
-    for freq,locs in antennas.items():
-        for c in combinations(locs,r=2):
+    for freq, locs in antennas.items():
+        for c in combinations(locs, r=2):
             print(c)
-            p1,p2 = find_antitodes(c[0],c[1])
-            if is_in_grid(p1,p_max=max_p):
+            p1, p2 = find_antitodes(c[0], c[1])
+            if is_in_grid(p1, p_max=max_p):
                 antitodes.add(p1)
-            if is_in_grid(p2,p_max=max_p):
+            if is_in_grid(p2, p_max=max_p):
                 antitodes.add(p2)
-    debug_print(antitodes, max_p, points)
     return len(antitodes)
 
 
@@ -40,24 +53,29 @@ def debug_print(antitodes, max_p, points):
     for y in range(max_p.y):
         for x in range(max_p.x):
             p = Point(x, y)
-            if p in points:
-                print(points[p], end="")
             if p in antitodes:
                 print("#", end="")
+            elif p in points:
+                print(points[p], end="")
             else:
                 print(".", end="")
         print()
 
 
-def part_2(lines):
-    pass
+def part_2(antennas, max_p, points):
+    antitodes = set()
+    for freq, locs in antennas.items():
+        for c in combinations(locs, r=2):
+            ants = find_antitodes2(c[0], c[1], max_p)
+            antitodes.update(ants)
+    return len(antitodes)
 
 
 def main():
     lines = get_lines("input_08.txt")
-    antennas, max_p,points = parse_input(lines)
-    print("Part 1:", part_1(antennas, max_p,points))
-    print("Part 2:", part_2(lines))
+    antennas, max_p, points = parse_input(lines)
+    print("Part 1:", part_1(antennas, max_p, points))
+    print("Part 2:", part_2(antennas, max_p, points))
 
 
 if __name__ == '__main__':
