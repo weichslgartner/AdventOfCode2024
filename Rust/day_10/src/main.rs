d214670117995ca37a537a25295ecb3b622df649
weichslgartner <weichslgartner@gmail.com>
2024-12-10 09:25:43 +0100

    refactor
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Rust/day_10/src/main.rs
+++ Rust/day_10/src/main.rs
@@ -1,17 +1,21 @@
+use aoc::{self, get_neighbours_4, Point};
 use std::collections::{HashMap, HashSet};
-use aoc::{self, Point, get_neighbours_4};
 
 fn parse_input(lines: Vec<&str>) -> (HashMap<Point, i32>, Vec<Point>, Point) {
     let mut points = HashMap::new();
     let mut starts = Vec::new();
-    
+
     for (y, line) in lines.iter().enumerate() {
         for (x, c) in line.chars().enumerate() {
+            let p = Point {
+                x: x as isize,
+                y: y as isize,
+            };
             if c != '.' {
-                points.insert(Point { x: x as isize, y: y as isize}, c.to_digit(10).unwrap() as i32);
+                points.insert(p, c.to_digit(10).unwrap() as i32);
             }
             if c == '0' {
-                starts.push(Point { x: x as isize, y: y as isize });
+                starts.push(p);
             }
         }
     }
@@ -33,7 +37,9 @@ fn get_n_trailheads(p: Point, p_max: Point, points: &HashMap<Point, i32>) -> (us
     while !cur.is_empty() {
         for &p in &cur {
             for n in get_neighbours_4(p, p_max).into_iter().filter(|n| {
-                points.get(n).map_or(false, |&value| value - points[&p] == 1)
+                points
+                    .get(n)
+                    .map_or(false, |&value| value - points[&p] == 1)
             }) {
                 if let Some(&value) = points.get(&n) {
                     if value == 9 {
@@ -53,7 +59,8 @@ fn get_n_trailheads(p: Point, p_max: Point, points: &HashMap<Point, i32>) -> (us
 }
 
 fn solve(points: &HashMap<Point, i32>, starts: &Vec<Point>, p_max: Point) -> (usize, i32) {
-    starts.iter()
+    starts
+        .iter()
         .map(|&p| get_n_trailheads(p, p_max, points))
         .fold((0, 0), |acc, (heads, score)| (acc.0 + heads, acc.1 + score))
 }
