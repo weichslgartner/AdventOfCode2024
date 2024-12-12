d2274edda026c2ba65f893b5ca5ccd4823858278
weichslgartner <weichslgartner@gmail.com>
2024-12-12 21:09:06 +0100

    day 12 part2
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_12.py
+++ Python/day_12.py
@@ -1,15 +1,15 @@
 import collections
-from typing import Set, List, Tuple
+from typing import Set, List, Tuple, Dict
 
 from aoc import get_lines, Point
 
 
-def parse_input(lines):
+def parse_input(lines: List[str]) -> Dict[str, Set[Point]]:
     regions = collections.defaultdict(set)
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
             regions[c].add(Point(x, y))
-    return regions, Point(x=len(lines[0]), y=len(lines))
+    return regions
 
 
 def get_neighbours_4(p: Point) -> Set[Point]:
@@ -20,7 +20,7 @@ def partition(c: str, points: Set[Point]) -> List[Tuple[str, Set[Point]]]:
     res = []
     while len(points) > 0:
         p = points.pop()
-        queue = get_neighbours_4(p)
+        queue = get_neighbours_4(p) & points
         region = {p}
         while len(queue) > 0:
             region |= queue & points
@@ -33,29 +33,47 @@ def partition(c: str, points: Set[Point]) -> List[Tuple[str, Set[Point]]]:
     return res
 
 
-def part_1(regions, p_max):
+def cnt_corners(p: Point, points: Set[Point]) -> int:
+    corners = 0
+    dirs = [(Point(-1, 0), Point(0, -1), Point(-1, -1)),
+            (Point(0, -1), Point(1, 0), Point(1, -1)),
+            (Point(1, 0), Point(0, 1), Point(1, 1)),
+            (Point(0, 1), Point(-1, 0), Point(-1, 1))]
+    for d in dirs:
+        p1 = Point(x=p.x + d[0].x, y=p.y + d[0].y)
+        p2 = Point(x=p.x + d[1].x, y=p.y + d[1].y)
+        p3 = Point(x=p.x + d[2].x, y=p.y + d[2].y)
+        if (p1 not in points) and (p2 not in points):
+            corners += 1
+        if p1 in points and p2 in points and p3 not in points:
+            corners += 1
+    return corners
+
+
+def solve(regions: Dict[str, Set[Point]]) -> (int, int):
     new_regions = []
     for c, points in regions.items():
-        new_regions += partition(c,points)
-    res = 0
+        new_regions += partition(c, points)
+    part1 = 0
+    part2 = 0
     for c, points in new_regions:
         perimeter = 0
-        for p in points:
+        n_corners = 0
+        for p in sorted(points):
             perimeter += len(get_neighbours_4(p).difference(points))
-       # print(c, len(points), perimeter, len(points) * perimeter)
-        res += len(points) * perimeter
-    return res
-
-
-def part_2(regions, p_max):
-    pass
+            n_corners += cnt_corners(p, points)
+        n = n_corners
+        part1 += len(points) * perimeter
+        part2 += len(points) * n
+    return part1, part2
 
 
 def main():
     lines = get_lines("input_12.txt")
     regions, p_max = parse_input(lines)
-    print("Part 1:", part_1(regions, p_max))
-    print("Part 2:", part_2(regions, p_max))
+    part1, part2 = solve(regions)
+    print("Part 1:", part1)
+    print("Part 2:", part2)
 
 
 if __name__ == '__main__':
