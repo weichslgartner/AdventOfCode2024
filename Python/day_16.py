7e88e107bb08907b9741190a471e99835384ac11
weichslgartner <weichslgartner@gmail.com>
2024-12-16 14:17:46 +0100

    day 16 part 2
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_16.py
+++ Python/day_16.py
@@ -27,23 +27,29 @@ def parse_input(lines):
     return walls, start, end
 
 
-def part_1(walls, start, end):
+def solve(walls, start, end):
     queue = []
-    heapq.heappush(queue, (0,Direction.EAST.value, (start, Direction.EAST)))
-
+    heapq.heappush(queue, (0,Direction.EAST.value, (start, Direction.EAST, [start])))
+    tiles_best_paths = set()
     costs_dict = collections.defaultdict(lambda: sys.maxsize)
     while len(queue) > 0:
-        cost,_, (point, direc) = heapq.heappop(queue)
+        cost,_, (point, direc, path) = heapq.heappop(queue)
         #print(cost, (point, direc) )
-        if cost >= costs_dict[(point,direc)]:
+        if cost > costs_dict[(point,direc)]:
             continue
         costs_dict[point,direc] = cost
         if point == end:
+            if cost < min(costs_dict[(end, d)] for d in Direction):
+                tiles_best_paths = set(path)
+            elif cost == min(costs_dict[(end, d)] for d in Direction):
+                tiles_best_paths.update(set(path))
             continue
         p_delta = dir_to_point(direc)
         next_p = Point(x=point.x + p_delta.x, y=point.y + p_delta.y)
         if next_p not in walls:
-            heapq.heappush(queue, (cost + 1,direc.value, (next_p, direc)))
+            path_tmp = path.copy()
+            path_tmp.append(next_p)
+            heapq.heappush(queue, (cost + 1,direc.value, (next_p, direc, path_tmp)))
         # Direction()
 
         for d in [Direction((direc.value + 1) % (len(Direction))),
@@ -51,9 +57,24 @@ def part_1(walls, start, end):
             p_delta = dir_to_point(d)
             next_p = Point(x=point.x + p_delta.x, y=point.y + p_delta.y)
             if next_p not in walls:
-                heapq.heappush(queue, (cost +1000 + 1, d.value, (next_p, d)))
+                path_tmp = path.copy()
+                path_tmp.append(next_p)
+                heapq.heappush(queue, (cost +1000 + 1, d.value, (next_p, d, path_tmp)))
+    #print_grid(tiles_best_paths, walls)
+    return min(costs_dict[(end,d)] for d in Direction), len(tiles_best_paths)
 
-    return min(costs_dict[(end,d)] for d in Direction)
+
+def print_grid(tiles_best_paths, walls):
+    for y in range(max(w.y for w in walls) + 1):
+        for x in range(max(w.x for w in walls) + 1):
+            p = Point(x, y)
+            if p in walls:
+                print("#", end="")
+            elif p in tiles_best_paths:
+                print("O", end="")
+            else:
+                print(".", end="")
+        print()
 
 
 def part_2(walls, start, end):
@@ -63,8 +84,9 @@ def part_2(walls, start, end):
 def main():
     lines = get_lines("input_16.txt")
     walls, start, end = parse_input(lines)
-    print("Part 1:", part_1(walls, start, end))
-    print("Part 2:", part_2(walls, start, end))
+    part1, part2 = solve(walls, start, end)
+    print("Part 1:", part1)
+    print("Part 2:", part2)
 
 
 if __name__ == '__main__':
