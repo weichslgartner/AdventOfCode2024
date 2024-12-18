c2a215ceb69da468cd2ae1bd37bca049821bac98
weichslgartner <weichslgartner@gmail.com>
2024-12-18 09:46:42 +0100

    day 18 binsearch added
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_18.py
+++ Python/day_18.py
@@ -9,7 +9,7 @@ def parse_input(lines):
     return [Point(*extract_all_ints(line)) for line in lines]
 
 
-def part_1(points, target=Point(70,70), n_nbytes=1024):
+def part_1(points, target=Point(70, 70), n_nbytes=1024):
     return find_min_path(n_nbytes, points, target)
 
 
@@ -24,7 +24,7 @@ def find_min_path(n_nbyes, points, target):
         cost, point = heapq.heappop(queue)
         if cost >= costs_dict[point]:
             continue
-        if cost > min(costs_dict[target] for d in Direction):
+        if cost > costs_dict[target]:
             continue
         costs_dict[point] = cost
         if point == target:
@@ -35,12 +35,21 @@ def find_min_path(n_nbyes, points, target):
     return None
 
 
-def part_2(points, target=Point(70,70), start_byte=1024):
-    for i in range(start_byte,len(points)):
-        if find_min_path(i, points, target) is None:
-            p=points[i-1]
-            return f"{p.x},{p.y}"
-    return ""
+def binary_search(l, r, points, target):
+    while l <= r:
+        m = (l + r) // 2
+        res = find_min_path(m, points, target)
+        if res is not None:
+            l = m + 1
+        else:
+            r = m - 1
+    return m
+
+
+def part_2(points, target=Point(70, 70), start_byte=1024):
+    i = binary_search(l=start_byte, r=len(points), points=points, target=target)
+    p = points[i - 1]
+    return f"{p.x},{p.y}"
 
 
 def main():
