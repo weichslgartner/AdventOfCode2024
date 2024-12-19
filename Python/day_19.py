315cbc5cdcf13723dea954581ab10426f8e0b5e9
weichslgartner <weichslgartner@gmail.com>
2024-12-19 09:22:06 +0100

    day 19 python refactor
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_19.py
+++ Python/day_19.py
@@ -10,29 +10,26 @@ def parse_input(input_str: str) -> (Tuple[str], List[str]):
 
 
 @cache
-def cnt_valid_designs(towels: Tuple[str], pattern: List[str]) -> int:
+def cnt_valid_designs(towels: Tuple[str], pattern: str) -> int:
     if len(pattern) == 0:
         return 1
-    total = 0
-    for t in towels:
-        if pattern.startswith(t):
-            total += cnt_valid_designs(towels, pattern[len(t):])
-    return total
+    valid_towels = filter(lambda t: pattern.startswith(t), towels)
+    return sum(cnt_valid_designs(towels, pattern[len(t):]) for t in valid_towels)
 
 
-def part_1(towels: Tuple[str], pattern: List[str]) -> int:
-    return sum(n for n in map(lambda x: x > 0, map(lambda p: cnt_valid_designs(towels, p), pattern)))
+def part_1(towels: Tuple[str], patterns: List[str]) -> int:
+    return sum(n for n in map(lambda x: x > 0, map(lambda p: cnt_valid_designs(towels, p), patterns)))
 
 
-def part_2(towels: Tuple[str], pattern: List[str]) -> int:
-    return sum(cnt_valid_designs(towels, p) for p in pattern)
+def part_2(towels: Tuple[str], patterns: List[str]) -> int:
+    return sum(cnt_valid_designs(towels, p) for p in patterns)
 
 
 def main():
     input_str = input_as_str("input_19.txt")
-    towels, pattern = parse_input(input_str)
-    print("Part 1:", part_1(towels, pattern))
-    print("Part 2:", part_2(towels, pattern))
+    towels, patterns = parse_input(input_str)
+    print("Part 1:", part_1(towels, patterns))
+    print("Part 2:", part_2(towels, patterns))
 
 
 if __name__ == '__main__':
