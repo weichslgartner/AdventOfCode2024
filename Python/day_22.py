8a6badff09e1aa35b46b698b43b6804ff7913311
weichslgartner <weichslgartner@gmail.com>
2024-12-22 17:25:48 +0100

    day 22 refactor
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- Python/day_22.py
+++ Python/day_22.py
@@ -1,31 +1,55 @@
+from functools import reduce
+from typing import List, Dict, Tuple
+
 from aoc import get_lines
 
+PATTERN_LENGTH = 4
 
-def parse_input(lines):
+
+def parse_input(lines: List[str]) -> List[int]:
     return [int(line) for line in lines]
 
 
-def do_operations(secret_number, rounds=2000):
+def calc_secret_numbers(secret_number: int, rounds: int = 2000) -> (int, List[int]):
+    prices = []
     for _ in range(rounds):
-        secret_number = (secret_number ^ secret_number * 64) % 16777216
-        secret_number = (secret_number ^ (secret_number // 32)) % 16777216
-        secret_number = (secret_number ^ (secret_number * 2048)) % 16777216
-    return secret_number
+        prices.append(secret_number % 10)
+        secret_number = (secret_number ^ (secret_number * 64 % 16777216))
+        secret_number = (secret_number ^ (secret_number // 32))
+        secret_number = (secret_number ^ (secret_number * 2048 % 16777216))
+    return secret_number, prices
 
 
-def part_1(numbers):
-    return sum(do_operations(n) for n in numbers)
+def calc_price_for_pattern(p: List[int]) -> Dict[Tuple, int]:
+    diffs = [i - j for i, j in zip(p[1:], p)]
+    pattern_dict = {}
+    for i in range(len(p) - PATTERN_LENGTH):
+        pattern = tuple(diffs[i:i + PATTERN_LENGTH])
+        if pattern not in pattern_dict:
+            pattern_dict[pattern] = p[i + PATTERN_LENGTH]
+    return pattern_dict
 
 
-def part_2(numbers):
-    pass
+def part_1(secrets: List[int]) -> int:
+    return sum(s for s in secrets)
+
+
+def part_2(prices: List[int]) -> int:
+    all_sequences = reduce(
+        lambda acc, p_dict: {key: acc.get(key, 0) + p_dict.get(key, 0)
+                             for key in acc.keys() | p_dict.keys()},
+        map(calc_price_for_pattern, prices),
+        {}
+    )
+    return max(all_sequences.values())
 
 
 def main():
     lines = get_lines("input_22.txt")
     numbers = parse_input(lines)
-    print("Part 1:", part_1(numbers))
-    print("Part 2:", part_2(numbers))
+    secrets = [calc_secret_numbers(n) for n in numbers]
+    print("Part 1:", part_1([secret for secret, _ in secrets]))
+    print("Part 2:", part_2([prices for _, prices in secrets]))
 
 
 if __name__ == '__main__':
